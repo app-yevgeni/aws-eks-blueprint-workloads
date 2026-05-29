fcd1adff6b1201ba5fbbf95d2852b9d7004d209d
app-yevgeni <23049337+app-yevgeni@users.noreply.github.com>
2026-05-30 02:23:08 +0300

    Add dependencies for EBS CSI addon

--- policy.tf
+++ policy.tf
@@ -42,7 +42,6 @@ resource "aws_iam_role_policy_attachment" "ebs_csi_irsa_policy" {
 }
 
 
-
 resource "aws_eks_addon" "ebs_csi" {
   cluster_name             = aws_eks_cluster.main.name
   addon_name               = "aws-ebs-csi-driver"
@@ -51,6 +50,8 @@ resource "aws_eks_addon" "ebs_csi" {
 
   depends_on = [
     aws_eks_cluster.main,
+    aws_eks_node_group.main,
+    aws_eks_node_group.app,
     aws_iam_role_policy_attachment.ebs_csi_irsa_policy
   ]
 }
