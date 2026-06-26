44eae3ba8683382233c77337b206033205021644
app-yevgeni <23049337+app-yevgeni@users.noreply.github.com>
2026-06-27 01:46:17 +0300

    Add HTTPD module configuration in resources.tf

---
+++ resources.tf
@@ -0,0 +1,12 @@
+###  ---  Default Application  ---  ###
+module "httpd" {
+  source = "./modules/httpd"
+  depends_on = [kubernetes_namespace.migration]
+
+  name   = "httpd-server"
+  namespace = "default"
+  replicas  = 2
+  image = "virtapp/apache:7f6c4bf4-3-6"
+  service_port = 8080
+  service_type = "ClusterIP"
+}
